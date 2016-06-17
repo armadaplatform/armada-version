48dfcc8002a0a5aba5f67fe86990af6f546b3d14
labuzm <marcin.labuz@gamedesire.eu>
2016-06-17 12:32:42 +0200

    change HIT type + small refactoring

--- src/handlers.py
+++ src/handlers.py
@@ -24,23 +24,9 @@ class IndexHandler(RequestHandler):
 class VersionCheckHandler(RequestHandler):
     async def get(self):
         client_version = self._validate_client_version()
-        if not client_version:
-            return
-
         # send version to GA, fire and forget
-        IOLoop.current().spawn_callback(self._collect_data, client_version)
-
-        try:
-            latest_version = await self._get_latest_version()
-        except HTTPError:
-            logging.exception('There was a problem with connecting to external service.')
-            return self.send_error(status_code=500, reason="Due to failure of third party services, "
-                                                           "we weren't able to handle your request.")
-
-        if not latest_version:
-            logging.error("Couldn't find any valid version number in armada's dockyard.")
-            return self.send_error(status_code=500, reason="We could't find any valid version candidate.")
-
+        IOLoop.current().spawn_callback(self._collect_data, client_version, self.request.remote_ip)
+        latest_version = await self._get_latest_version()
         data = {
             'latest_version': str(latest_version),
             'is_newer': latest_version > client_version
@@ -48,24 +34,30 @@ class VersionCheckHandler(RequestHandler):
         self.write(json_encode(data))
 
     async def _get_latest_version(self):
-        response = await http_client.fetch('http://192.168.3.158:5001/v2/armada/tags/list')
+        try:
+            response = await http_client.fetch('http://192.168.3.158:5001/v2/armada/tags/list')
+        except HTTPError:
+            logging.exception('There was a problem with connecting to external service.')
+            raise HTTPError(code=500, message="Due to failure of third party services, "
+                                              "we weren't able to handle your request.")
         tags = json_decode(response.body)['tags']
-
         try:
             return max(self._get_valid_versions(tags))
         except ValueError:
-            pass
+            logging.error("Couldn't find any valid version number in armada's dockyard.")
+            raise HTTPError(code=500, message="We could't find any valid version candidate.")
 
     @staticmethod
-    async def _collect_data(client_version):
+    async def _collect_data(client_version, ip_address):
         data = {
             'v': 1,
-            't': 'event',
+            't': 'screenview',
             'tid': config['tid'],
             'cid': str(uuid.uuid4()),
-            'ec': 'armada-version',
-            'ea': 'version-check',
-            'el': str(client_version)
+            'an': 'armada-version',
+            'cd': 'version-check',
+            'av': str(client_version),
+            'uip': ip_address  # uip is anonymized by default
         }
 
         body = urlencode(data)
@@ -83,11 +75,10 @@ class VersionCheckHandler(RequestHandler):
                 pass
 
     def _validate_client_version(self):
-        client_version = self.get_argument('version', None)
-        if client_version is None:
-            return self.send_error(status_code=400, reason='Version number not provided.')
-
+        client_version = self.get_argument('version')
+        if not client_version:
+            raise HTTPError(code=400, message='Missing argument version.')
         try:
             return StrictVersion(client_version)
         except ValueError:
-            return self.send_error(status_code=400, reason='Invalid version number.')
+            raise HTTPError(code=400, message='Invalid version number.')
