1dc9843e5a7204a8c2e73916945a74ac184dcae3
labuzm <labuzm@gmail.com>
2016-07-07 07:04:18 +0200

    add timeouts

--- src/handlers.py
+++ src/handlers.py
@@ -3,10 +3,11 @@ import logging
 from urllib.parse import urlencode
 
 from armada import hermes
+from tornado.gen import Task
 from tornado.ioloop import IOLoop
 from tornado.escape import json_decode
 from tornado.web import RequestHandler, HTTPError
-from tornado.httpclient import AsyncHTTPClient, HTTPError as HTTPClientError
+from tornado.httpclient import AsyncHTTPClient, HTTPError as HTTPClientError, HTTPRequest
 
 from utils import status, StrictVerboseVersion
 
@@ -25,7 +26,7 @@ class VersionCheckHandler(RequestHandler):
     async def get(self):
         client_version = self._validate_client_version()
         # send version to GA, fire and forget
-        #IOLoop.current().spawn_callback(self._collect_data, client_version, self.request.remote_ip)
+        IOLoop.current().spawn_callback(self._collect_data, client_version, self.request.remote_ip)
         latest_version = await self._get_latest_version()
         data = {
             'latest_version': str(latest_version),
@@ -33,9 +34,17 @@ class VersionCheckHandler(RequestHandler):
         }
         self.write(data)
 
+    @staticmethod
+    async def make_request(url, method='GET', body=None, timeout=None):
+        request = HTTPRequest(url, method, body=body, request_timeout=timeout)
+        response = await Task(http_client.fetch, request)
+        if response.error:
+            raise response.error
+        return response
+
     async def _get_latest_version(self):
         try:
-            response = await http_client.fetch('http://dockyard.armada.sh/v2/armada/tags/list')
+            response = await self.make_request('http://dockyard.armada.sh/v2/armada/tags/list', timeout=3)
         except HTTPClientError:
             logging.exception('There was a problem with connecting to external service.')
             raise HTTPError(500, reason="Due to failure of third party services, "
@@ -47,8 +56,7 @@ class VersionCheckHandler(RequestHandler):
             logging.error("Couldn't find any valid version number in armada's dockyard.")
             raise HTTPError(500, reason="We could't find any valid version candidate.")
 
-    @staticmethod
-    async def _collect_data(client_version, ip_address):
+    async def _collect_data(self, client_version, ip_address):
         data = {
             'v': 1,
             't': 'screenview',
@@ -62,7 +70,7 @@ class VersionCheckHandler(RequestHandler):
 
         body = urlencode(data)
         try:
-            await http_client.fetch('https://www.google-analytics.com/collect', method='POST', body=body)
+            await self.make_request('https://www.google-analytics.com/collect', method='POST', body=body, timeout=5)
         except HTTPClientError:
             logging.exception('An error occurred while trying to send Google Analytics.')
 
